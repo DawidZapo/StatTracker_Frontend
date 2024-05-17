1c41361209f0d7546479a4b2645aaa374fabb5f4
DawidZapo <dawidzapo@gmail.com>
2024-05-17 18:10:16 +0200

    Deleted div, minor change

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,58 @@
 <template>
   <div id="app">
-<!--    <nav class="navbar navbar-expand navbar-dark bg-dark">-->
-<!--      <div class="navbar-nav mr-auto">-->
-<!--        <ul>-->
-<!--          <li class="nav-item">-->
-<!--            <router-link to="/home" class="nav-link">Home</router-link>-->
-<!--          </li>-->
-<!--          <li v-if="showAdminBoard" class="nav-item">-->
-<!--            <router-link to="/admin" class="nav-link">Admin Board</router-link>-->
-<!--          </li>-->
-<!--          <li v-if="showModeratorBoard" class="nav-item">-->
-<!--            <router-link to="/mod" class="nav-link">Moderator Board</router-link>-->
-<!--          </li>-->
-<!--          <li class="nav-item">-->
-<!--            <router-link v-if="currentUser" to="/user" class="nav-link">User</router-link>-->
-<!--          </li>-->
-<!--          <li>-->
-<!--            <router-link to="/court">Court</router-link>-->
-<!--          </li>-->
-<!--        </ul>-->
-<!--      </div>-->
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <div class="navbar-nav mr-auto">
+        <ul>
+          <li class="nav-item">
+            <router-link to="/home" class="nav-link">Home</router-link>
+          </li>
+          <li v-if="showAdminBoard" class="nav-item">
+            <router-link to="/admin" class="nav-link">Admin Board</router-link>
+          </li>
+          <li v-if="showModeratorBoard" class="nav-item">
+            <router-link to="/mod" class="nav-link">Moderator Board</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link v-if="currentUser" to="/user" class="nav-link">User</router-link>
+          </li>
+          <li>
+            <router-link to="/court">Court</router-link>
+          </li>
+        </ul>
+      </div>
 
-<!--      <div v-if="!currentUser" class="navbar-nav ml-auto">-->
-<!--        <ol>-->
-<!--          <li class="nav-item">-->
-<!--            <router-link to="/register" class="nav-link">-->
-<!--              Sign Up-->
-<!--            </router-link>-->
-<!--          </li>-->
-<!--          <li class="nav-item">-->
-<!--            <router-link to="/login" class="nav-link">-->
-<!--              Login-->
-<!--            </router-link>-->
-<!--          </li>-->
-<!--        </ol>-->
-<!--      </div>-->
+      <div v-if="!currentUser" class="navbar-nav ml-auto">
+        <ol>
+          <li class="nav-item">
+            <router-link to="/register" class="nav-link">
+              Sign Up
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">
+              Login
+            </router-link>
+          </li>
+        </ol>
+      </div>
 
-<!--      <div v-if="currentUser" class="navbar-nav ml-auto">-->
-<!--        <ol>-->
-<!--          <li class="nav-item">-->
-<!--            <router-link to="/profile" class="nav-link">-->
-<!--              {{ currentUser.username }}-->
-<!--            </router-link>-->
-<!--          </li>-->
-<!--          <li class="nav-item">-->
-<!--            <a class="nav-link" href @click.prevent="logOut">-->
-<!--              LogOut-->
-<!--            </a>-->
-<!--          </li>-->
-<!--        </ol>-->
-<!--      </div>-->
-<!--    </nav>-->
+      <div v-if="currentUser" class="navbar-nav ml-auto">
+        <ol>
+          <li class="nav-item">
+            <router-link to="/profile" class="nav-link">
+              {{ currentUser.username }}
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href @click.prevent="logOut">
+              LogOut
+            </a>
+          </li>
+        </ol>
+      </div>
+    </nav>
 
-    <div class="container" style="background-color: black">
+    <div class="container">
       <router-view/>
     </div>
   </div>
