e73b70e10e2743a5bd821b2d6aab9c4069af2807
DawidZapo <dawidzapo@gmail.com>
2024-06-18 11:49:55 +0200

    Prior to changes

--- src/components/game_routes/GameCreation.vue
+++ src/components/game_routes/GameCreation.vue
@@ -3,6 +3,7 @@
 import {computed, ref, watch} from "vue";
 import TeamService from "@/services/team/team.service.js";
 import {useStore} from "vuex";
+import GameCreated from "@/models/game/GameCreated.js";
 
 const today = new Date().toISOString().slice(0, 10);
 const now = new Date();
@@ -57,6 +58,29 @@ const selectedHomePlayersForGame = ref([]);
 const selectedHomeStartingFive = ref([]);
 const selectedAwayPlayersForGame = ref([]);
 const selectedAwayStartingFive = ref([]);
+
+const gameData = ref({
+  id: null,
+  date: '',
+  time: '',
+  season: '',
+  official: '',
+  quarterLengthMin: '',
+  home: {
+    id: null,
+    name: '',
+    players: [
+      { id: null, firstName: '', lastName: '', shirtNumber: null, startingFive: false }
+    ]
+  },
+  away: {
+    id: null,
+    name: '',
+    players: [
+      { id: null, firstName: '', lastName: '', shirtNumber: null, startingFive: false }
+    ]
+  }
+});
 const isHomeSelectedForGame = (id) =>{
   return selectedHomePlayersForGame.value.includes(id);
 };
@@ -130,140 +154,149 @@ const handleAwayClick = () => {
   fetchTeam(selectedAwayId.value, false);
 };
 
+
+const handleSubmit = () => {
+  const gameCreated = new GameCreated(gameData.value);
+  console.log(gameCreated);
+};
+
 </script>
 
 <template>
   <div class="container shadow-lg">
-    <div class="d-flex justify-content-center">
-      <h5 class="mt-4">Game Details</h5>
-    </div>
-    <hr class="my-2">
-    <div class="container text-center">
-      <div class="row">
-        <div class="col">
-          <div class="row d-flex align-items-center justify-content-center mx-4">
+    <form @submit.prevent="handleSubmit">
+      <div class="d-flex justify-content-center">
+        <h5 class="mt-4">Game Details</h5>
+      </div>
+      <hr class="my-2">
+      <div class="container text-center">
+        <button type="submit" class="btn btn-success">Create</button>
+        <div class="row">
+          <div class="col">
+            <div class="row d-flex align-items-center justify-content-center mx-4">
               <h6 class="mx-1">Date</h6>
-              <input type="date" class="small-text custom-input text-center" v-model="myDate">
+              <input type="date" class="small-text custom-input text-center" v-model="gameData.date">
+            </div>
           </div>
-        </div>
-        <div class="col">
-          <div class="row d-flex align-items-center justify-content-center mx-4">
-            <h6 class="mx-1">Time</h6>
-            <input type="time" class="small-text custom-input text-center" v-model="myTime">
+          <div class="col">
+            <div class="row d-flex align-items-center justify-content-center mx-4">
+              <h6 class="mx-1">Time</h6>
+              <input type="time" class="small-text custom-input text-center" v-model="gameData.time">
+            </div>
           </div>
-        </div>
-        <div class="col">
-          <div class="row d-flex align-items-center justify-content-center mx-4">
+          <div class="col">
+            <div class="row d-flex align-items-center justify-content-center mx-4">
               <h6 class="mx-1">Season</h6>
-              <select class="small-text custom-input text-center">
-                <option v-for="season in seasons">{{season}}</option>
+              <select class="small-text custom-input text-center" v-model="gameData.season">
+                <option v-for="season in seasons" :value="season">{{season}}</option>
               </select>
+            </div>
           </div>
-        </div>
-        <div class="col">
-          <div class="row d-flex align-items-center justify-content-center justify-content-center mx-4">
+          <div class="col">
+            <div class="row d-flex align-items-center justify-content-center justify-content-center mx-4">
               <h6 class="mx-1">Official</h6>
-              <input type="checkbox" class="small-text text-center">
+              <input type="checkbox" class="small-text text-center" v-model="gameData.official">
+            </div>
           </div>
-        </div>
-        <div class="col">
-          <div class="row d-flex align-items-center justify-content-center mx-4">
+          <div class="col">
+            <div class="row d-flex align-items-center justify-content-center mx-4">
               <h6 class="mx-1">Quarter</h6>
-              <input type="number" class="small-text custom-input text-center">
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="d-flex justify-content-center mt-4">
-      <h5 class="mt-2">Teams</h5>
-    </div>
-    <hr class="my-2">
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          <div class="card">
-            <div class="card-header">
-              <select id="homeSelect" class="small-text custom-input text-center" @change="handleHomeClick">
-                <option selected disabled>Please select home team</option>
-                <option v-for="team in teams" :value="team.id">{{team.name}} </option>
-              </select>
-            </div>
-            <div class="card-body">
-              <template v-if="selectedHome !== null">
-                <table class="table table-striped table-hover table-bordered small-text text-center">
-                  <thead>
-                  <tr>
-                    <th scope="col">Selected</th>
-                    <th scope="col">Player</th>
-                    <th scope="col">S5</th>
-                    <th scope="col">Shirt no.</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tr v-for="(player,index) in selectedHome.currentPlayers">
-                    <td style="width: 10%; align-content: center">
-                      <input type="checkbox" :value="player.id" v-model="selectedHomePlayersForGame">
-                    </td>
-                    <td style="align-content: center" :class="{'disabled' : !isHomeSelectedForGame(player.id)}">
-                      {{ player.firstName + ' ' + player.lastName }}
-                    </td>
-                    <td style="width: 10%; align-content: center" :class="{'disabled' : !isHomeSelectedForGame(player.id)}">
-                      <input type="checkbox" :value="player.id" v-model="selectedHomeStartingFive">
-                    </td>
-                    <td style="width: 20%; align-content: center" :class="{'disabled' : !isHomeSelectedForGame(player.id)}">
-                      <input type="number" class="custom-input">
-                    </td>
-                  </tr>
-                  </tbody>
-                </table>
-                <div v-show="!isValidHome" class="alert alert-danger small-text" style="padding: 3px; text-align: center">Adjust lineup</div>
-              </template>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card">
-            <div class="card-header">
-              <select id="awaySelect" class="small-text custom-input text-center" @change="handleAwayClick">
-                <option disabled selected>Please select away team</option>
-                <option v-for="team in teams" :value="team.id">{{team.name}}</option>
-              </select>
-            </div>
-            <div class="card-body">
-              <template v-if="selectedAway !== null">
-                <table class="table table-striped table-hover table-bordered small-text text-center">
-                  <thead>
-                  <tr>
-                    <th scope="col">Selected</th>
-                    <th scope="col">Player</th>
-                    <th scope="col">S5</th>
-                    <th scope="col">Shirt no.</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tr v-for="(player,index) in selectedAway.currentPlayers">
-                    <td style="width: 10%; align-content: center">
-                      <input type="checkbox" :value="player.id" v-model="selectedAwayPlayersForGame">
-                    </td>
-                    <td style="align-content: center" :class="{'disabled' : !isAwaySelectedForGame(player.id)}">
-                      {{ player.firstName + ' ' + player.lastName }}
-                    </td>
-                    <td style="width: 10%; align-content: center" :class="{'disabled' : !isAwaySelectedForGame(player.id)}">
-                      <input type="checkbox" :value="player.id" v-model="selectedAwayStartingFive">
-                    </td>
-                    <td style="width: 20%; align-content: center" :class="{'disabled' : !isAwaySelectedForGame(player.id)}">
-                      <input type="number" class="custom-input">
-                    </td>
-                  </tr>
-                  </tbody>
-                </table>
-                <div v-show="!isValidAway" class="alert alert-danger small-text" style="padding: 3px; text-align: center">Adjust lineup</div>
-              </template>
+              <input type="number" class="small-text custom-input text-center" v-model="gameData.quarterLengthMin">
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div class="d-flex justify-content-center mt-4">
+        <h5 class="mt-2">Teams</h5>
+      </div>
+      <hr class="my-2">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="card">
+              <div class="card-header">
+                <select id="homeSelect" class="small-text custom-input text-center" @change="handleHomeClick">
+                  <option selected disabled>Please select home team</option>
+                  <option v-for="team in teams" :value="team.id">{{team.name}} </option>
+                </select>
+              </div>
+              <div class="card-body">
+                <template v-if="selectedHome !== null">
+                  <table class="table table-striped table-hover table-bordered small-text text-center">
+                    <thead>
+                    <tr>
+                      <th scope="col">Selected</th>
+                      <th scope="col">Player</th>
+                      <th scope="col">S5</th>
+                      <th scope="col">Shirt no.</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="(player,index) in selectedHome.currentPlayers">
+                      <td style="width: 10%; align-content: center">
+                        <input type="checkbox" :value="player.id" v-model="selectedHomePlayersForGame">
+                      </td>
+                      <td style="align-content: center" :class="{'disabled' : !isHomeSelectedForGame(player.id)}">
+                        {{ player.firstName + ' ' + player.lastName }}
+                      </td>
+                      <td style="width: 10%; align-content: center" :class="{'disabled' : !isHomeSelectedForGame(player.id)}">
+                        <input type="checkbox" :value="player.id" v-model="selectedHomeStartingFive">
+                      </td>
+                      <td style="width: 20%; align-content: center" :class="{'disabled' : !isHomeSelectedForGame(player.id)}">
+                        <input type="number" class="custom-input">
+                      </td>
+                    </tr>
+                    </tbody>
+                  </table>
+                  <div v-show="!isValidHome" class="alert alert-danger small-text" style="padding: 3px; text-align: center">Adjust lineup</div>
+                </template>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <div class="card-header">
+                <select id="awaySelect" class="small-text custom-input text-center" @change="handleAwayClick">
+                  <option disabled selected>Please select away team</option>
+                  <option v-for="team in teams" :value="team.id">{{team.name}}</option>
+                </select>
+              </div>
+              <div class="card-body">
+                <template v-if="selectedAway !== null">
+                  <table class="table table-striped table-hover table-bordered small-text text-center">
+                    <thead>
+                    <tr>
+                      <th scope="col">Selected</th>
+                      <th scope="col">Player</th>
+                      <th scope="col">S5</th>
+                      <th scope="col">Shirt no.</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="(player,index) in selectedAway.currentPlayers">
+                      <td style="width: 10%; align-content: center">
+                        <input type="checkbox" :value="player.id" v-model="selectedAwayPlayersForGame">
+                      </td>
+                      <td style="align-content: center" :class="{'disabled' : !isAwaySelectedForGame(player.id)}">
+                        {{ player.firstName + ' ' + player.lastName }}
+                      </td>
+                      <td style="width: 10%; align-content: center" :class="{'disabled' : !isAwaySelectedForGame(player.id)}">
+                        <input type="checkbox" :value="player.id" v-model="selectedAwayStartingFive">
+                      </td>
+                      <td style="width: 20%; align-content: center" :class="{'disabled' : !isAwaySelectedForGame(player.id)}">
+                        <input type="number" class="custom-input">
+                      </td>
+                    </tr>
+                    </tbody>
+                  </table>
+                  <div v-show="!isValidAway" class="alert alert-danger small-text" style="padding: 3px; text-align: center">Adjust lineup</div>
+                </template>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
---
+++ src/models/game/GameCreated.js
@@ -0,0 +1,34 @@
+import {createPercentageComputed} from "@/assets/scripts/stats.js";
+
+class Player {
+    constructor(data) {
+        this.id = data.id;
+        this.firstName = data.firstName;
+        this.lastName = data.lastName;
+        this.shirtNumber = data.shirtNumber;
+        this.startingFive = data.startingFive;
+    }
+}
+
+class Team {
+    constructor(data) {
+        this.id = data.id;
+        this.name = data.name;
+        this.players = data.players.map(dataPlayer => new Player(dataPlayer));
+    }
+}
+
+class GameCreated {
+    constructor(data) {
+        this.id = data.id;
+        this.date = data.date;
+        this.time = data.time;
+        this.season = data.season;
+        this.official = data.official;
+        this.quarterLengthMin = data.quarterLengthMin;
+        this.home = new Team(data.home);
+        this.away = new Team(data.away);
+    }
+}
+
+export default GameCreated;
